134c13ada0376d7daa12ccff64d30963f1f1ee4a
prabaltomar11 <prabalpratapsinghtomar11@gmail.com>
2026-07-12 12:28:36 +0530

    Mastered Python Variables and Data Types

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("hello")
+my_name = "prabal"
+bca_year = 2 
+
+print("My name is", my_name, "and I am in", bca_year, "year of BCA.")
+
+
+a = 15 
+b = 4 
+rem = a % b
+print(rem)
+
+
+price = 99.99
+print(type(price))
