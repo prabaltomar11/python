a91c11c86879f48e2ea0790a59d14663089731a2
prabaltomar11 <prabalpratapsinghtomar11@gmail.com>
2026-07-13 14:14:01 +0530

    Mastered Pattern Printing and Nested Loops

--- hello.py
+++ hello.py
@@ -1,11 +1,4 @@
-num = int(input("enter number:"))
-for count in range(1,11):
-    print("table:", num * count)
-
-
-
-
-for i in range(1,21):
-    if i % 2 == 0:
-      print( i)
-   
+for i in range(1, 4):         
+    for j in range(1, i + 1): 
+        print(j, end=" ")   
+    print()
