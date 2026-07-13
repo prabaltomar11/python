91ab3888e6f89bdad9ba833a3fee7023ec448306
prabaltomar11 <prabalpratapsinghtomar11@gmail.com>
2026-07-13 12:15:25 +0530

    Mastered Python While Loop and Table Generator

--- hello.py
+++ hello.py
@@ -1,40 +1,15 @@
-color = input("enter color: ")
-if color == "red":
-   print("Stop!")
-elif color == "yello":
-   print("Wait!")
-elif color == "green":
-   print("Go!")
-else :
-   print("Invalid Color.")
+i = 10
+while i >= 1:
+    print("reverse:", i)
+    i -=1
 
 
 
-num1 = int(input("enter number: "))
-if num1 % 2 == 0:
-   print("Even")
-
-else :
-   print("Odd")
-
-
-
-
-num2 = int(input("enter number: "))
-if num2 > 0:
-   print("Positive")
-
-elif num2 < 0:
-   print("Negative")
-
-else :
-   print("Zero")      
-
-
-
-
-
-
+num = int(input("enter number:"))
+count = 1
+while count <= 10:
+    print(num * count)
+    count += 1
 
 
 
