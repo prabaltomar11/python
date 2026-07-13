789d14e4b27ca19202d68ac730010f4f42dbded5
prabaltomar11 <prabalpratapsinghtomar11@gmail.com>
2026-07-13 12:53:38 +0530

    Mastered Python For Loops and Nested Conditions

--- hello.py
+++ hello.py
@@ -1,24 +1,11 @@
-i = 10
-while i >= 1:
-    print("reverse:", i)
-    i -=1
-
-
-
 num = int(input("enter number:"))
-count = 1
-while count <= 10:
-    print(num * count)
-    count += 1
+for count in range(1,11):
+    print("table:", num * count)
 
 
 
 
-
-
-
-
+for i in range(1,21):
+    if i % 2 == 0:
+      print( i)
    
-
-
-
