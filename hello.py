83004b5e7f51281c4e4154edd320b1bd1a13d1ac
prabaltomar11 <prabalpratapsinghtomar11@gmail.com>
2026-07-14 11:56:34 +0530

    Mastered Python Functions and def keyword

--- hello.py
+++ hello.py
@@ -1,4 +1,17 @@
-for i in range(1, 4):         
-    for j in range(1, i + 1): 
-        print(j, end=" ")   
-    print()
+def greet_hero(name):
+    print(name, "this is the best hero!")
+
+greet_hero("Spider-Man")
+
+
+
+
+def multiply_three(a, b, c):
+    answere = a * b * c
+    return answere
+
+result = multiply_three(2, 3, 4)
+print(result)
+
+
+
